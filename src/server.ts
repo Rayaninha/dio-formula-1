cd4844029c50773707dd268240431aa37d2e694f
Rayaninha <salesrayane11@gmail.com>
2024-07-12 11:23:46 -0300

    feat: simple server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,20 @@
-console.log("hello world");
+import fastify from "fastify";
+
+const server = fastify({ logger: true });
+
+server.get("/teams", async (request, response) => {
+  response.type("application/json").code(200);
+
+  return [
+    {
+      id: 1,
+      name: "ferrari",
+    },
+  ];
+});
+
+const port = parseInt(process.env.PORT || "3333");
+
+server.listen({ port }, () => {
+  console.log("Server init");
+});
